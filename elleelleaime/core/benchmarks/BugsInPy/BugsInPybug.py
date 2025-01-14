f335bdf240ad7072eaee0cf3c7eeee5e7c2601d4
Tom Sorger <tomsorger314@gmail.com>
2025-01-14 13:32:08 +0100

    add test implementation for BugsInPybug

--- elleelleaime/core/benchmarks/BugsInPy/BugsInPybug.py
+++ elleelleaime/core/benchmarks/BugsInPy/BugsInPybug.py
@@ -42,7 +42,7 @@ class BugsInPyBug(Bug):
 
         # Checkout the bug
         checkout_run = subprocess.run(
-            f"{self.benchmark.get_bin()}checkout -p {self.project_name} -v {self.version_id} -i {self.bug_id} -w {path}",
+            f"{self.benchmark.get_bin()}bugsinpy-checkout -p {self.project_name} -v {self.version_id} -i {self.bug_id} -w {path}",
             shell=True,
             capture_output=True,
             check=True,
@@ -60,9 +60,36 @@ class BugsInPyBug(Bug):
 
     def compile(self, path: str) -> CompileResult:
         run = subprocess.run(
-            f"cd {path} && timeout {5*60} {self.benchmark.get_bin()}compile",
+            f"cd {path} && timeout {5*60} {self.benchmark.get_bin()}bugsinpy-compile",
             shell=True,
             capture_output=True,
             check=True,
         )
         return CompileResult(run.returncode == 0, run.stdout, run.stderr)
+
+    def test(self, path: str) -> TestResult:
+        # First run only relevant tests
+        run = subprocess.run(
+            f"cd {path} && timeout {30*60} {self.benchmark.get_bin()}bugsinpy-test",
+            shell=True,
+            capture_output=True,
+            check=False,
+        )
+
+        pattern = r"FAIL: ([\w_.]+ \([\w_.]+\))"
+        m = re.findall(pattern, run.stdout.decode("utf-8"))
+
+        if not (run.returncode == 0 and m != None and int(m.group(1)) == 0):
+            return TestResult(False)
+        return TestResult(run.returncode == 0 and m != None and int(m.group(1)) == 0)
+
+    # TODO: Implement later
+    # def get_src_test_dir(self, path: str) -> str:
+    #     run = subprocess.run(
+    #         f"cd {path} && {self.benchmark.get_bin()} export -p dir.src.tests",
+    #         shell=True,
+    #         capture_output=True,
+    #         check=True,
+    #     )
+
+    #     return run.stdout.decode("utf-8").strip()
